00e79ad58374f641435d129aa8856b3854859173
ndios <natalia.dco@gmail.com>
2014-07-21 18:49:27 +0200

    Matrix cache v1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to calculate matrix inverses and maintain them on cache for efficiency
 
-## Write a short comment describing this function
-
+## Create, maintain and manage a matrix cache
 makeCacheMatrix <- function(x = matrix()) {
-
+		i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(mean) i <<- mean
+        getinverse <- function() i
+		
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Calculate matrix inverse or get from cache if exists
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse()
+		if (!is.null(i)) {
+			message("Getting cached data")
+			return(i)
+		}
+		
+		m <- x$get()
+        
+        # square matrix?
+		if(nrow(m)/ncol(m) == 1) {
+            i <- solve(m)
+            x$setinverse(i)
+		}
+		else {
+			print("Hey! The matrix is supposed to be square...")
+		}
+		
+        i
 }
